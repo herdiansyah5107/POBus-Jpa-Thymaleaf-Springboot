48f594e8154541955275639ae91ff6a6a93f1cf1
herdiansyah5107@gmail <herdiansyah5107@gmail.com>
2021-12-03 22:39:05 +0700

    menambahkan controller cancel booking

---
+++ src/main/java/com/busspringboot/controller/cancelBookingController.java
@@ -0,0 +1,34 @@
+package com.busspringboot.controller;
+
+import com.busspringboot.model.Booking;
+import com.busspringboot.repository.BookingRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class cancelBookingController {
+    
+    @Autowired
+    BookingRepository bookingRepo;
+    
+    //Membatalkan booking
+	@GetMapping("/cancel")
+	public String cancelBooking(Model model) {
+	model.addAttribute("dataBooking", new Booking());
+	return "formcancel";
+		}
+	
+	@PostMapping("/cancelBooking")
+	public String BookingCancel(@ModelAttribute("dataBooking")Booking dataBooking,
+	Model model){
+	long id = dataBooking.getId(); 
+	bookingRepo.deleteById(id);
+	return "cancelmassage";
+	}
+}
